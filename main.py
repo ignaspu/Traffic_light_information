89100cc751f073ef03f8692fb61bfc1a9486d9bf
ignaspu <pupelis.ignas@gmail.com>
2024-04-25 19:51:20 +0300

    second task

--- main.py
+++ main.py
@@ -6,4 +6,12 @@ data = pandas.read_csv('data.csv')
 red_numbers = data.Red.sum()
 yellow_numbers = data.Yellow.sum()
 green_numbers = data.Green.sum()
-print(f"Red: {red_numbers}, yellow: {yellow_numbers}, green: {green_numbers}")
+print("Numbers of each colors occurrences:")
+print(f"Red: {red_numbers}, yellow: {yellow_numbers}, green: {green_numbers}\n")
+
+# 2. Find how long each colour was active for.
+active = data[["Red", "Yellow", "Green"]].multiply(data.TimeActive, 0).sum()
+print(f"Each color was active for:")
+for color, time in active.items():
+    print(f"{color}={time} seconds")
+
