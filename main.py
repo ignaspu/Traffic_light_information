41836a111f85e47682cb1845d35c4397649ff3a1
ignaspu <pupelis.ignas@gmail.com>
2024-04-25 20:12:18 +0300

    fourth task

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ print(f"Red: {red_numbers}, yellow: {yellow_numbers}, green: {green_numbers}\n")
 
 # 2. Find how long each colour was active for.
 active = data[["Red", "Yellow", "Green"]].multiply(data.TimeActive, 0).sum()
-print(f"Each color was active for:")
+print("Each color was active for:")
 for color, time in active.items():
     print(f"{color}={time} seconds")
 
@@ -20,3 +20,13 @@ green_active_time = data[data.Green == 1].Time.tolist()
 print("Green was active in these times:")
 for each in green_active_time:
     print(each)
+
+# 4. Find the number of complete cycles Red-Yellow-Green-Yellow-Red in the data
+cycles = (
+    (data.Red == 1) &
+    (data.Yellow.shift(-1) == 1) &
+    (data.Green.shift(-2) == 1) &
+    (data.Yellow.shift(-3) == 1) &
+    (data.Red.shift(-4) == 1)
+).sum()
+print(f"Number of complete cycles: {cycles}")
